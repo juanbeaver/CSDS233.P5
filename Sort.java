1a02969c04bc4602e2e5230ed70fe0f23fcccae6
Juan Beaver <jcarlosbeaver@live.com>
2022-04-15 14:58:44 -0400

    see log

--- Sort.java
+++ Sort.java
@@ -3,7 +3,7 @@ public class Sort {
     public static void main(String[] args) {
         int[] arr = randomArray(10,0,10);
 
-        mergeSort(arr);
+        quickSort(arr);
     }
 
     static public void insertionSort(int[] arr){
@@ -62,43 +62,49 @@ public class Sort {
 
 
     static public void mergeSort(int[] arr){
-        int[] temp = new int[arr.length];
-        mSort(arr, temp, 0 , arr.length - 1);
-    }
+        if(arr.length < 2)  return;
 
-    static private void mSort(int[] arr, int[] temp, int start, int end){
-        if(start == end){
-            return;
+        int middle = arr.length/2;
+
+        int[] left = new int[middle];
+        int[] right = new int[arr.length - middle];
+
+        for (int i = 0; i < middle; i++) {
+            left[i] = arr[i];
         }
-        int middle = (start + end)/2;
 
-        mSort(arr, temp, start, middle);
-        mSort(arr, temp, middle + 1, end);
-        merge(arr, temp, start, middle, middle +1, end);
+        for (int i = middle; i < arr.length; i++) {
+            right[i - middle] = arr[i];
+        }
+
+        mergeSort(left);
+        mergeSort(right);
+        merge(arr, left, right);
     }
 
-    static public void merge(int[] arr, int[] temp, int leftStart, int leftEnd, int rightStart, int rightEnd){
-        int i = leftStart;
-        int j = rightStart;
-        int k = leftStart;
+    static public void merge(int[] arr, int[] left, int[] right){
+        int i = 0, j = 0, k = 0; //left, right, and arr trackers.
 
-        while (i <= leftEnd && j <= rightEnd){
-            if(arr[i] < arr[j]){
-                temp[k++] = arr[i++];
+        while(i < left.length && j < right.length){
+            if(left[i] <= right[j]){
+                arr[k] = left[i];
+                i++;
             }
             else{
-                temp[k++] = arr[i++];
+                arr[k] = right[j];
+                j++;
             }
+            k++;
         }
-        while(i <= leftEnd){
-            temp[k++] = arr[i++];
+        while(j < right.length){
+            arr[k] = right[j];
+            j++;
+            k++;
         }
-        while(j <= rightEnd){
-            temp[k++] = arr[j++];
-        }
-
-        for(i = leftStart; i <= rightEnd; i++) {
-            arr[i] = temp[i];
+        while(i < left.length){
+            arr[k] = left[i];
+            k++;
+            i++;
         }
     }
 
